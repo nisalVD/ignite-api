f1f7c268fda2b5864df84c33f162b48695e75ddb
nisalVD <nisalvd@gmail.com>
2017-11-26 23:43:58 +1100

    removed prettifier as i wasn't using that anymore

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const express = require('express');
 const server = express();
 
 server.listen(7000, ()=>{
-    console.log('Started at http://localhost:7000')
+    console.log('Started at http://localhost:7000');
 });
