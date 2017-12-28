e8acd6d6b9fc6ecd5fb0865319745e4fe661106a
nisalVD <nisalvd@gmail.com>
2017-12-28 19:59:46 +1100

    add bodyparser as middleware and cleaned up eslint

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const server = express();
+
+// Middleware Plugins
+server.use(bodyParser())
+
 
 server.listen(7000, ()=>{
     console.log('Started at http://localhost:7000');
