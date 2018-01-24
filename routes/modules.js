6fee1c11f01eef09f31030a159ae1baf61130a67
nisalVD <nisalvd@gmail.com>
2018-01-24 19:55:43 +1100

    add find by module id route

--- routes/modules.js
+++ routes/modules.js
@@ -11,4 +11,20 @@ router.get('/modules', authMiddleware.requireJWT, (req, res) => {
   .catch(err => res.status(404).send(err))
 })
 
+router.get('/module/:id' , authMiddleware.requireJWT, (req,res) => {
+  Module.findById(req.params.id)
+  .then(foundModule => {
+    if (foundModule){
+      res.status(202).json(foundModule)
+    } else {
+      res.status(404).json({
+        error: new Error(`user with ${id} not found`)
+      })
+    }
+  })
+  .catch((error) => {
+    res.status(400).json({ error: error.message }) 
+  })
+})
+
 module.exports = router
