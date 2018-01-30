b08e9496357be0db1a3b7514b648063241817428
nisalVD <nisalvd@gmail.com>
2018-01-30 21:49:04 +1100

    changed the names to heading content

--- models/Feed.js
+++ models/Feed.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const FeedSchema = new Schema({
-  name : {
+  heading : {
     type: String,
     required: true
   },
-  description : {
+  content : {
     type: String,
     required: true
   },
