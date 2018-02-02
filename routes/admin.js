bd1f829af81e7b6ad2b329fb2a3621e315dc9847
nisalVD <nisalvd@gmail.com>
2018-02-02 18:45:51 +1100

    categorised admin routes with comments

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,8 @@ const Marking = require('../models/Marking')
 const requireAdmin = require('../middleware/admin')
 
 const router = new express.Router()
+
+//## Module Routes
 // Find All user data
 router.get('/users', requireAdmin,  (req,res) => {
   User.find()
@@ -38,6 +40,7 @@ router.delete('/module/:id', requireAdmin, (req, res) => {
     })
 })
 
+//## Question Routes
 // List all questions
 router.get('/questions', requireAdmin, (req, res) => {
   Question.find()
@@ -49,20 +52,6 @@ router.get('/questions', requireAdmin, (req, res) => {
 router.post('/question', requireAdmin, (req,res) => {
   Question.create(req.body)
     .then(question => res.status(201).json(question))
-    .catch(err => res.send(err))
-})
-
-// List all the answers
-router.get('/answers', requireAdmin, (req, res) => {
-  Answer.find()
-  .then(answer => res.status(202).json(answer))
-  .catch(err => res.status(404).send(err))
-})
-
-// Add new answers
-router.post('/answer', requireAdmin, (req,res) => {
-  Answer.create(req.body)
-    .then(answer => res.status(201).json(answer))
     .catch(err => res.send(err))
 })
 
@@ -82,32 +71,31 @@ router.delete('/question/:id', requireAdmin, (req,res) => {
       res.status(404).json({error: error.message})
     })
 })
-
-// delete answer by question id
-router.delete('/question/:id/answer', requireAdmin, (req ,res) => {
-  const {id} = req.params
-  Answer.remove({question: id})
-  .then(question => {
-    if(question){
-      res.status(202).json(question)
-    }
-    else {
-      res.status(404).json(`question with ${id} not found`)
-    }
-  })
-  .catch(error => {
-    res.status(404).json({error: error.message})
-  })
+ 
+//## Answer Routes
+// List all the answers
+router.get('/answers', requireAdmin, (req, res) => {
+  Answer.find()
+  .then(answer => res.status(202).json(answer))
+  .catch(err => res.status(404).send(err))
 })
 
+// Add new answers
+router.post('/answer', requireAdmin, (req,res) => {
+  Answer.create(req.body)
+    .then(answer => res.status(201).json(answer))
+    .catch(err => res.send(err))
+})
+
+//## Marking Routes
 router.get('/markings', requireAdmin, (req,res) => {
   Marking.find()
     .then(markings => res.status(202).json(markings))
     .catch(error => res.status(500).json(error))
 })
 
-// feed 
-
+//## Feed Routes
+// feeds routes
 router.post('/feed', (req,res) => {
   Feed.create(req.body)
     .then(newsFeed => res.status(202).json(newsFeed))
