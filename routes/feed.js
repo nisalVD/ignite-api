86500b2c1696715ac0574b64864ad8a7e8815230
nisalVD <nisalvd@gmail.com>
2018-02-02 18:42:05 +1100

    add auth middle and remvoved post router as its a admin route

--- routes/feed.js
+++ routes/feed.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const Feed = require('../models/Feed')
+const authMiddleware = require('../middleware/auth')
 
 const router = new express.Router()
 
-router.get('/feeds', (req,res) => {
+router.get('/feeds', authMiddleware.requireJWT, (req,res) => {
   Feed.find()
     .then(feeds => res.status(202).json(feeds))
     .catch(error => res.status(404).send(error))
 })
 
-router.post('/feed', (req,res) => {
-  Feed.create(req.body)
-    .then(newsFeed => res.status(202).json(newsFeed))
-    .catch(error => res.status(404).send(error))
-})
 module.exports = router
