f663f42cedaea3f568a2cb6e7493c44496b37d49
nisalVD <nisalvd@gmail.com>
2018-02-04 20:47:10 +1100

    patch to change password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const authMiddleWare = require('../middleware/auth')
+const User = require('../models/User')
+const getUser = require('../middleware/getUser')
 
 const router = new express.Router()
 
@@ -14,5 +16,18 @@ router.post('/auth',
   authMiddleWare.signIn,
   authMiddleWare.signJWTForUser
 )
+
+// update User
+router.patch('/auth/update', authMiddleWare.requireJWT, getUser, (req, res) => {
+  User.findById(req.sub)
+    .then(user => {
+      user.changePassword(req.body.oldPassword,req.body.newPassword, function(err) {
+        if (err){
+            return res.status(200).json({success: false})
+        }
+        return res.status(404).json({sucess: true})
+      })
+    })
+})
 
 module.exports = router
