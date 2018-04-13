7cd82bc122fffa5e59a920d954786abd0d8b42f8
nisalVD <nisalvd@gmail.com>
2018-04-13 18:51:05 +1000

    Add mailgun email sending thing

--- middleware/getUser.js
+++ middleware/getUser.js
@@ -1,14 +1,13 @@
 const jwt = require('jsonwebtoken')
 
 const jwtSecret = process.env.JWT_SECRET
-console.log('jwtSecret', jwtSecret)
 const jwtAlgorithm = process.env.JWT_ALGORITHM
-console.log('jwtAlgorithm', jwtAlgorithm )
 
 function getUser(req, res, next) {
   const token = req.headers['authorization'].split(' ')[1]
   jwt.verify(token , jwtSecret,{ algorithms: [jwtAlgorithm] }, function(err, decoded) {
     req.sub = decoded.sub
+    req.decoded = decoded
     next()
   })
 
--- routes/userDetails.js
+++ routes/userDetails.js
@@ -2,6 +2,10 @@ const express = require('express')
 const User = require('../models/User')
 const getUser = require('../middleware/getUser')
 
+const api_key = process.env.MAILGUN_KEY
+const domain = 'sandbox7a44e8b99eae406fa91ee0ecd9054406.mailgun.org'
+const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
 const router = new express.Router()
 
 router.patch('/user/password/update',getUser, (req, res) => {
@@ -28,5 +32,23 @@ router.patch('/user/details/update', getUser, (req, res) => {
     .catch(error => res.status(404).json(error))
 })
 
+router.post('/user/check-list-complete', getUser, (req, res) => {
+  const data = {
+    from: 'Excited User <nisalvd@gmail.com>',
+    to: 'nisalvd@gmail.com',
+    subject: 'All Tests Completed',
+    text: `All tests have been completed by user with the email ${req.decoded.email}`
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    if (error) {
+      res.status(404).json({error})
+    } else {
+      res.status(200).json({body})
+    }
+
+  })
+
+})
 
 module.exports = router
